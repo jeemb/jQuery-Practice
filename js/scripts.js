cfc40003d399f18a02c9e6e6bfb8f2f52c9909ca
jb <jenniferbeem@gmail.com>
2017-01-10 10:28:40 -0800

    add jQuery functions to click on text and add/remove image

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,6 @@
 $(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
+  $(".clickable").click(function() {
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
   });
-
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  $("img").click(function() {
-    alert("This is an image.");
-  });
-
-	var sentence = prompt("What sentence would you like to encode?");
-	var stringLength = sentence.length;
-
-	var first = function(sentence) {
-		return (sentence.charAt(0)+sentence.charAt(sentence.length-1));
-	};
-
-	var last = function(sentence) {
-		return (sentence.charAt(sentence.length-1)+sentence.charAt(0));
-	};
-
-	alert(first(sentence));
-	alert(last(sentence));
-	alert(last(first(sentence)));
 });
